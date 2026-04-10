3911613b4b3a69fb6651d3e139be061fe2436a20
Andreas <andreas@familie-lanz.de>
2026-04-10 22:19:25 +0200

    Remove unnecessary logging in setup

--- src/main.cpp
+++ src/main.cpp
@@ -15,23 +15,10 @@
 
 Knx2HovalGatewayModule hovalGatewayModule;
 
-const uint8_t firmwareRevision = 1;
-
 void setup()
 {
   // Start debug serial with 115200 bauds
   Serial.begin(115200);
-#if defined(OPENKNX_DEBUG)
-  // Wait up to 5 seconds for serial to be ready
-  uint8_t i = 0;
-  while (!Serial && i++ < 5)
-  {
-    delay(1000);
-  }
-
-  logInfo("KNX2HovalGateway", "r%u %s", firmwareRevision, __TIMESTAMP__);
-  Serial.flush();
-#endif
 
   openknx.init(firmwareRevision);
 
